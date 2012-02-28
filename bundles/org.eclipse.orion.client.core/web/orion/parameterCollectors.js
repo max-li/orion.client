f9e76f3acf477f035e1f67fd4a1848f760c446dc
Max Li <maxli@ca.ibm.com>
2012-02-28 13:02:02 -0500

    Bug 372758 - Allow use of spacebar to activate all buttons

--- bundles/org.eclipse.orion.client.core/web/orion/parameterCollectors.js
+++ bundles/org.eclipse.orion.client.core/web/orion/parameterCollectors.js
@@ -116,7 +116,7 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/util', 'dijit/Menu'
 					}));
 					// onClick events do not register for spans when using the keyboard without a screen reader
 					dojo.connect(close, "onkeypress", dojo.hitch(this, function (e) {
-						if(e.keyCode === dojo.keys.ENTER) {
+						if(e.keyCode === dojo.keys.ENTER || e.charCode === dojo.keys.SPACE) {
 							this.close(this._activeElements);
 						}
 					}));
@@ -231,7 +231,7 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/util', 'dijit/Menu'
 					}));
 					// onClick events do not register for spans when using the keyboard without a screen reader
 					dojo.connect(options, "onkeypress", dojo.hitch(this, function (e) {
-						if(e.keyCode === dojo.keys.ENTER) {			
+						if(e.keyCode === dojo.keys.ENTER  || e.charCode === dojo.keys.SPACE) {			
 							commandInvocation.parameters.optionsRequested = true;
 							finish(this);
 						}
@@ -249,7 +249,7 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/util', 'dijit/Menu'
 				}));
 				// onClick events do not register for spans when using the keyboard without a screen reader
 				dojo.connect(ok, "onkeypress", dojo.hitch(this, function (e) {
-					if(e.keyCode === dojo.keys.ENTER) {
+					if(e.keyCode === dojo.keys.ENTER  || e.charCode === dojo.keys.SPACE) {
 						finish(this);
 					}
 				}));
@@ -266,7 +266,7 @@ define(['require', 'dojo', 'dijit', 'orion/commands', 'orion/util', 'dijit/Menu'
 				}));
 				// onClick events do not register for spans when using the keyboard without a screen reader
 				dojo.connect(close, "onkeypress", dojo.hitch(this, function (e) {
-					if(e.keyCode === dojo.keys.ENTER) {
+					if(e.keyCode === dojo.keys.ENTER  || e.charCode === dojo.keys.SPACE) {
 						localClose();
 					}
 				}));
